fa7529dfe48a150b7733c475d5eb059e7277e80c
Rana Ashraf <55793250+RanaAshraf2000@users.noreply.github.com>
2021-01-30 15:31:17 +0200

    Update test.java
    1) private : private modifier is only within the class It cannot be accessed from outside the class
    اي متغير بعده لا يستخدم خارج الكلاس
    2) Protected: 
    subclass  يستخدم داخل جميع الاكلاسات المشتقه
    packageو جميع الكلاسات اللي في نفس
    3) Default :  يعني مش بحدد 
    access modifier 
    قبل المتغير و المتغير ميقدرش يوصله غير
     classes 
    اللي في نفس  
    package 
    
    4) public :  عام يعني باختصار تستخدم في اي مكان حتى 
    classes
    اللي في نفس
    package

--- Task 2/test.java
+++ Task 2/test.java
@@ -1,3 +1,18 @@
 /*
-Try to upload your java file here like this file. 
+1) private : private modifier is only within the class It cannot be accessed from outside the class
+اي متغير بعده لا يستخدم خارج الكلاس
+2) Protected: 
+subclass  يستخدم داخل جميع الاكلاسات المشتقه
+packageو جميع الكلاسات اللي في نفس
+3) Default :  يعني مش بحدد 
+access modifier 
+قبل المتغير و المتغير ميقدرش يوصله غير
+ classes 
+اللي في نفس  
+package 
+
+4) public :  عام يعني باختصار تستخدم في اي مكان حتى 
+classes
+اللي في نفس
+package
 */
